d44c132fe00567c09c81e146101475e313e07f38
Abrar <abrarmurad00@gmail.com>
2025-09-29 19:47:26 -0400

    preline install

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,8 @@
 import Image from "next/image";
-<<<<<<< HEAD
-import Navbar from "../src/landing/navbar";
-import Hero from "../src/landing/hero";
-import About from "../src/landing/about";
-import Contact from "../src/landing/contact";
-=======
 import Navbar from "./landing/navbar";
 import Hero from "./landing/hero";
 import About from "./landing/about";
 import Contact from "./landing/contact";
->>>>>>> 15a22ad3616a0aa82f6cc8e3b9f1068a98bb5b67
 
 export default function Home() {
   return (
