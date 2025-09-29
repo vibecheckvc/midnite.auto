d6acf330c2883aab87fbf095ecaf48e2963df90e
Abrar <abrarmurad00@gmail.com>
2025-09-29 19:58:30 -0400

    routing

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
-import Navbar from "./landing/navbar";
-import Hero from "./landing/hero";
-import About from "./landing/about";
-import Contact from "./landing/contact";
+import Link from "next/link";
+import Navbar from "../src/landing/navbar";
+import Hero from "../src/landing/hero";
+import About from "../src/landing/about";
+import Contact from "../src/landing/contact";
 
 export default function Home() {
   return (
@@ -11,6 +12,21 @@ export default function Home() {
       <Hero />
       <About />
       <Contact />
+
+      <ul>
+        <li>
+          <Link href="/garage/123">Garage Overview (ID 123)</Link>
+        </li>
+        <li>
+          <Link href="/garage/cars">Cars</Link>
+        </li>
+        <li>
+          <Link href="/garage/settings">Settings</Link>
+        </li>
+        <li>
+          <Link href="/garage/history">History</Link>
+        </li>
+      </ul>
     </div>
   );
 }
